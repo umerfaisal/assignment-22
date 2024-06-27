671fca0315611fe725cdc75ae36d095f05734cf0
umerfaisal <109797391+umerfaisal@users.noreply.github.com>
2024-06-27 20:17:16 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+//Making Array
+var indexError = ["A", "B", "C", "D"];
+// Producing Error
+console.log(indexError[10]);
+// Fixing the error
+console.log(indexError[1]);
---
+++ main.ts
@@ -0,0 +1,8 @@
+//Making Array
+let indexError = ["A","B","C","D"]
+
+// Producing Error
+// console.log(indexError[10]);
+
+// Fixing the error
+console.log(indexError[1]);
